2b1183d7ff1d828e176602e04c5e2a195a30ab40
Gareth Hudson <garethhudson07@gmail.com>
2021-01-26 12:28:44 +0000

    Update config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -13,11 +11,5 @@ module.exports = {
     },
     devServer: {
         contentBase: './dist',
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Development area',
-        }),
-    ],
+    }
 };
